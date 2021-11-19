b0031c74b5778bd20276e8a3f0c76baf89d4eeb1
Park Minseon <msp1125@hanyang.ac.kr>
2021-11-20 01:32:25 +0900

    2021-11-18

---
+++ check_react_speed/ResponseCheck.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div id="screen" v-bind:class="state">{{message}}</div>
+        <div>
+            <div>평균 시간: {{}}</div>
+            <button v-on:click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                result: [],
+                state: 'waiting',
+                message: '클릭해서 시작하세요.',
+            }
+        },
+        methods: {
+            
+        },
+    };
+</script>
+
+<style>
+    #screen {
+        width: 300px;
+        height: 200px;
+        text-align: center;
+        user-select: none;
+    }
+    #screen.waiting {
+        background-color: aqua;
+    }
+    #screen.ready {
+        background-color: red;
+        color: white;
+    }
+    #screen.now {
+        background-color: greenyellow;
+    }
+</style>
---
+++ check_react_speed/webpack.config.js
@@ -0,0 +1,30 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+// webpacking: 스크립트를 하나로 합치기 위해 사용
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+    entry: {
+        // 모든 script를 합칠 대표 파일 지정
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: {
+        // 자바스크립트 파일을 합칠 때 어떤 규칙을 따를 것?
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        // dist라는 폴더 내에 app.js 라는 최종 결과물이 나올 것
+        filename: 'app.js', // = [name].js
+        path: path.join(__dirname, 'dist'),
+    },
+};
